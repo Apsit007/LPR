ba2386b7f3a9a2bb5db1686fbd508675a9e77d52
apisit:name <apisit@multita.co.th>
2024-10-28 13:58:48 +0700

    Add : unplugicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import UnpluginIcons from "unplugin-icons/vite";
+import UnpluginFonts from "unplugin-fonts/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    UnpluginIcons({
+      jsx: "react",
+      compiler: "jsx",
+      autoInstall: true
+    }),
+    UnpluginFonts({
+      google: {
+        families: [
+          {
+            name: "Sarabun",
+            styles: "wght@300;400;500;600;700;800;900;1000",
+          },
+          {
+            name: "Noto Sans Thai",
+            styles: "wght@300;400;500;600;700;800;900;1000",
+          },
+          {
+            name: "Kanit",
+            styles: "wght@300;400;500;600;700;800;900;1000",
+          },
+        ],
+      },
+    }),
+
+  ],
 })
